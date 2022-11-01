87701bc6afcec947a4ed6f756e35fde45fbc1aeb
N-a290 <nietoangel2902@gmail.com>
2022-10-31 20:30:56 -0500

    Creando BlogView y generando su url

---
+++ frontend/src/views/BlogView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>BLOG</h1>
+  </div>
+  <div>
+    <div v-for="post in APIdata" :key="post.id">
+      <div>
+        <p>{{ post.title }}</p>
+        <p>{{ post.excerpt }}</p>
+      </div>
+    </div>
+  </div>
+</template>
