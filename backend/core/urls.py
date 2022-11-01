9c52601ac3109de153c00f19d785be2890eaf689
N-a290 <nietoangel2902@gmail.com>
2022-10-31 19:10:17 -0500

    Creando los serializadores y las vistas de la API, creando frontend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -7,6 +7,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('blog/', include('blog.urls', namespace="blog")),
+
     path('api-auth/', include('rest_framework.urls')),
 ]
 
